ac8837df777171477c9d315565f771b0ce72f3b7
LukaszPasich <lukas.zed81@gmail.com>
2025-07-26 21:50:01 +0000

    FEAT: Add scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,24 @@ $(document).ready(function() {
   console.log('jQuery is working!');
 });
 
-// Current Year in Copyright
+
+// CURRENT YEAR IN COPYRIGHT SECTION
 document.getElementById("year").textContent = new Date().getFullYear();
+
+
+// SCROLL TO TOP BUTTON
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// Show button when scrolling down
+window.onscroll = () => {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+// Scroll to top when clicked
+scrollBtn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};

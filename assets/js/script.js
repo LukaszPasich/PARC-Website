e1c3be536ee48d59b24124a1956968ec41ba924f
LukaszPasich <68754236+LukaszPasich@users.noreply.github.com>
2025-08-12 22:36:49 +0000

    FEAT: Add images to all 13 testimonial slides

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,7 +111,7 @@ const testimonials = [
   },
   {
     // testimonial 2
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-2.jpg",
     imageAlt: "Image of Angie at PCHEI 2024 Conference",
     header: "PCHEI Conference 2024",
     body: `I thoroughly enjoyed the workshop and felt challenged to think about this fascinating area in a compassionate space.`,
@@ -119,7 +119,7 @@ const testimonials = [
   },
   {
     // testimonial 3
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-3.jpg",
     imageAlt: "Image of Angie at PSI 2023 Workshop",
     header: "PSI Workshop 2023",
     body: `The way it was delivered, the reflection space created by the facilitator and the exercises. Angie was very engaging.`,
@@ -127,7 +127,7 @@ const testimonials = [
   },
   {
     // testimonial 4
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-4.jpg",
     imageAlt: "Image of Angie at PSI 2023 Workshop",
     header: "PSI Workshop 2023",
     body: `Informal and inclusive atmosphere, discussion and opportunity to explore the real life implications of EDI.`,
@@ -135,7 +135,7 @@ const testimonials = [
   },
   {
     // testimonial 5
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-5.jpg",
     imageAlt: "Image of Liliana at PSI 2023 Workshop",
     header: "PSI Workshop 2023",
     body: `I thought Liliana’s way of presenting the information was brilliant. She was able to communicate key concepts in an easily understood manner.
@@ -144,7 +144,7 @@ const testimonials = [
   },
   {
     // testimonial 6
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-6.jpg",
     imageAlt: "Image of Liliana at PSI 2023 Workshop",
     header: "PSI Workshop 2023",
     body: `More training like this would be WELCOMED on the course. It was fantastic.`,
@@ -152,7 +152,7 @@ const testimonials = [
   },
   {
     // testimonial 7
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-7.jpg",
     imageAlt: "Image of Liliana at PSI 2023 Workshop",
     header: "PSI Workshop 2023",
     body: `Excellent workshop… It was very thought provoking and worthwhile.
@@ -161,7 +161,7 @@ const testimonials = [
   },
   {
     // testimonial 8
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-8.jpg",
     imageAlt: "Image of Liliana and Angie",
     header: "PSI Workshop 2023",
     body: `“I expected a lecture, but I got pleasantly surprised by the presentation and enjoyed myself.
@@ -170,7 +170,7 @@ const testimonials = [
   },
   {
     // testimonial 9
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-9.jpg",
     imageAlt: "Image of Liliana and Angie at IAHIP 2024 Conference",
     header: "IAHIP Conference 2024",
     body: `“I really enjoyed the opening segment with Liliana and Angie. It was engaging, interesting and well presented.`,
@@ -178,7 +178,7 @@ const testimonials = [
   },
   {
     // testimonial 10
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-8.jpg",
     imageAlt: "Image of Liliana and Angie",
     header: "IAHIP Conference 2024",
     body: `I also enjoyed the input in the morning session which was of a high standard, thought provoking and relevant.`,
@@ -186,7 +186,7 @@ const testimonials = [
   },
   {
     // testimonial 11
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-9.jpg",
     imageAlt: "Image of Angie at IAHIP 2024 Conference",
     header: "IAHIP Conference 2024",
     body: `The day was well structured, initial presentation around discrimination was excellent.`,
@@ -194,7 +194,7 @@ const testimonials = [
   },
   {
     // testimonial 12
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-8.jpg",
     imageAlt: "Image of Liliana and Angie",
     header: "IAHIP Conference 2024",
     body: `Angie and Liliana’s presentation was so good and very relevant.`,
@@ -202,7 +202,7 @@ const testimonials = [
   },
   {
     // testimonial 13
-    image: "assets/images/testimonial-1.jpg",
+    image: "assets/images/testimonial-9.jpg",
     imageAlt: "Image of Angie at IAHIP 2024 Conference",
     header: "IAHIP Conference 2024",
     body: `The two speakers presenting Diversity within mental health were exceptionally interesting and I enjoyed the talk very much.`,
